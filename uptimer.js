acf92cb485f0a2c3c0fceabf7a96de166c507f58
chimp <55600314+thischimp@users.noreply.github.com>
2020-09-24 02:30:51 +0300

    Add files via upload

---
+++ uptimer.js
@@ -0,0 +1,116 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const chalk = require("chalk");
+var Jimp = require("jimp");
+const { Client, Util } = require("discord.js");
+const fs = require("fs");
+const http = require("http");
+const express = require("express");
+const kontrol = require("node-fetch");
+const data = require('quick.db');
+require("./util/eventLoader")(client);
+
+client.ayarlar = { "token": "TOKEN", "prefix": "PREFİX", "sahip": "SAHİP" };
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.error(err);
+  console.log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./commands/${f}`);
+    console.log(`Yüklenen komut: ${client.ayarlar.prefix}${props.help.name}`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      let cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./commands/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      let cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 1;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 2;
+  if (message.author.id === message.guild.owner.id) permlvl = 3;
+  if (message.author.id === client.ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g
+
+client.login(client.ayarlar.token);
+
+client.on('ready', async () => {
+client.user.setActivity(`Özelden: ${client.ayarlar.prefix}link ekle/liste/sil url/hepsi | Can°B#1308 tarafından CodAre için yapılmıştır.`)
+client.user.setStatus('dnd');
+});
+
+setInterval(() => {
+const linkler = data.fetch('chimped');
+if(linkler) {
+if(linkler.length > 0) {
+linkler.forEach(s => {
+kontrol(s.site).catch(err => {
+console.log('');
+console.log(`${s.site} hata verdi. Sahibi: ${s.sahipTag}`);
+console.log('');
+})
+console.log(`${s.site} uptime edildi. Sahibi: ${s.sahipTag}`);
+})
+}
+}
+}, 60000)
